68f3d1863982b0b740ff55086860ee779d7afa5d
nicolamamo <62040103+nicolamamo@users.noreply.github.com>
2020-04-22 08:23:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// chiedo a utente sua mail
+
+
+var mailUtente = prompt("iserisci e mail")
+
+console.log(mailUtente);
+
+// controllo se è dentro mail list
+
+var listaMail = ['enzo.benzo@gmail.com', 'quartoquinto@gmail.com', 'settimio.severo@gmail.com']
+console.log(listaMail);
+for (var i = 0; i < listaMail.length; i++) {
+    listaMail[i]
+}
+
+if (mailUtente=listaMail[i]) {
+    console.log(la tua mail è valida);
+
+}
+
+else {
+    console.log(mail non valida);
+}
